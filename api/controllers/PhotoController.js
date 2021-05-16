6b85fa44ede79055c61f09c0ba1dc0eb7cd50f8a
Aurelio D'Urso <aurelio@MBPdiAurelio2.homenet.telecomitalia.it>
2021-05-16 16:54:01 +0200

    sistemazioni

--- api/controllers/PhotoController.js
+++ api/controllers/PhotoController.js
@@ -5,6 +5,8 @@ let Photo = mongoose.model('Photo')
 
 exports.listAllPhotos = function (req, res) {
 
+  console.log('mostro')
+
   Photo.find(function (err, photo) {
     if (err) res.send(err);
     res.json(photo);
